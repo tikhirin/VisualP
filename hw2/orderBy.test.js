e2e366cfeb52cbd3c73ecc56c7cf86981bf47f0a
rinettechi <tikhirin71@gmail.com>
2025-02-24 14:40:02 +0700

    edit orderby.js

--- hw2/orderBy.test.js
+++ hw2/orderBy.test.js
@@ -2,15 +2,15 @@ const orderBy = require('./orderBy');
 
 test('sorts objects by given properties', () => {
     const data = [
-        { name: 'Boothill', age: 25 },
-        { name: 'Aventurine', age: 30 },
-        { name: 'Ratio', age: 22 }
+        { name: 'John', age: 25 },
+        { name: 'Alice', age: 30 },
+        { name: 'John', age: 22 }
     ];
     const sorted = orderBy(data, ['name', 'age']);
     expect(sorted).toEqual([
-        { name: 'Aventurine', age: 30 },
-        { name: 'Ratio', age: 22 },
-        { name: 'Ratio', age: 25 }
+        { name: 'Alice', age: 30 },
+        { name: 'John', age: 22 },
+        { name: 'John', age: 25 }
     ]);
 });
 
@@ -24,7 +24,7 @@ test('throws error if array contains non-object elements', () => {
 
 test('throws error if some objects lack required properties', () => {
     const data = [
-        { name: 'Ratio' },
+        { name: 'John' },
         { age: 25 }
     ];
     expect(() => orderBy(data, ['name'])).toThrow("Property 'name' is missing in some objects");
