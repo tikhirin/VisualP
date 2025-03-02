21783156e3d4ef272bc8a0e698216ada3ac7ea4d
rinettechi <164432179+tikhirin@users.noreply.github.com>
2025-03-03 01:44:04 +0800

    Delete hw3/index.js

--- hw3/index.js
+++
@@ -1,8 +0,0 @@
-const calcStatsFromAPI = require('./calcStatsFromAPI');
-
-async function main() {
-    const stats = await calcStatsFromAPI();
-    console.log('Статистика по странам:', stats);
-}
-
-main()
